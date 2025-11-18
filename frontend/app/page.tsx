95b3ed417ae952614fbf628291b9a7f69b93f629
JahleelT <jlt9287@nyu.edu>
2025-11-18 03:00:02 -0500

    added business logo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,13 +2,22 @@ import About from "./components/About";
 import Booking from "./components/Booking";
 import Footer from "./components/Footer";
 import Services from "./components/Services";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <main className="flex flex-col items-center">
 
-        <section className="w-full h-[60vh] flex items-center justify-center bg-gray-100">
+        <section className="relative w-full h-[60vh] flex items-center justify-center bg-gray-100 overflow-hidden">
+
+          <Image
+            src="/notary_logo.png"
+            alt="Good Signs Notary Logo"
+            width={400}
+            height={400}
+            className="absolute opacity-10 -z-10 pointer-events-none object-contain left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+          />
           <h1 className="text-4xl font-bold">Welcome to Good Signs Notary Services</h1>
         </section>
 
